602e933a7d4a8f0e8d20663aa907f78bfdf513e5
Lars Hudson Lunde <laffen47@yahoo.no>
2025-05-06 20:08:46 +0200

    Create sar-deb.sh

---
+++ sar-deb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OUTPUT="disk_stats.json"
+ARCHIVE="disk_stats.zip"
+START_DATE=$(date --date="1 month ago" +%d)
+END_DATE=$(date +%d)
+
+> "$OUTPUT"
+echo "[" >> "$OUTPUT"
+
+for i in $(seq -w $START_DATE $END_DATE); do
+    FILE="/var/log/sysstat/sa$i"
+    if [ -f "$FILE" ]; then
+        sadf -j "$FILE" -- -dp | jq '.sysstat.hosts[0].statistics' >> "$OUTPUT"
+        echo "," >> "$OUTPUT"
+    fi
+done
+
+# Remove trailing comma and close array
+sed -i '$ s/,$//' "$OUTPUT"
+echo "]" >> "$OUTPUT"
+
+zip -9 "$ARCHIVE" "$OUTPUT"
+base64 -w 0 "$ARCHIVE"
+
